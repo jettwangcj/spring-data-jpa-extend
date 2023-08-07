6aedf15b8999aef4a7529aed6289824b47c78e10
wangchangjiu <wangchangjiu@rr.tv>
2023-08-07 21:25:01 +0800

    springboot 3.x jpa pom

--- src/main/java/cn/org/wangchangjiu/jpa/extend/ExpressionQueryResolverStrategy.java
+++ src/main/java/cn/org/wangchangjiu/jpa/extend/ExpressionQueryResolverStrategy.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import cn.hutool.core.util.StrUtil;
 
 /**
  * @Classname ExpressionQueryResolver
